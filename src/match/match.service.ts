5a81d2441bd77e43325c2fc501a79f5004f90c38
Maxence.p <maxence.pille@kandixi.com>
2025-02-05 09:07:12 +0100

    initial

--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { FirebaseService } from 'src/config/firebase.config';
-
 @Injectable()
 export class MatchService {
-    private db: FirebaseFirestore.Firestore
+  private db: FirebaseFirestore.Firestore;
 
-    constructor(private readonly firebaseService: FirebaseService) {
-        this.db = firebaseService.getFirestore();
-    }
+  constructor(private readonly firebaseService: FirebaseService) {
+    this.db = firebaseService.getFirestore();
+  }
 
-    async findAll() {
-        const snapshot = await this.db.collection('match').get();
-        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    }
+  async findAll() {
+    const snapshot = await this.db.collection('match').get();
+    return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+  }
 
-    async findById(id: string) {
-        const doc = await this.db.collection('matches').doc(id).get();
-        return doc.exists ? { id: doc.id, ...doc.data() } : null;
-    }
+  async findById(id: string) {
+    const doc = await this.db.collection('matches').doc(id).get();
+    return doc.exists ? { id: doc.id, ...doc.data() } : null;
+  }
 
-    async createMatch(data: any) {
-        const docRef = await this.db.collection('match').add(data);
-    
-        return { id: docRef.id, ...data };
-    }
+  async createMatch(data: any): Promise<any> {
+    const docRef = await this.db.collection('match').add(data);
+
+    return { id: docRef.id, ...data };
+  }
 }
