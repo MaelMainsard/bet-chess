439a77b268df56542d18c7d47c8fe289e850b293
Necati ÜNLÜ <tati440@hotmail.fr>
2025-02-05 08:49:07 +0100

    ajout de la liaison entre firestore et les matchs

--- src/match/match.controller.ts
+++ src/match/match.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MatchService } from './match.service';
 
 @Controller('match')
-export class MatchController {}
+export class MatchController {
+    constructor(private readonly matchService: MatchService) {}
+
+    @Get()
+    findAll() {
+        return this.matchService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.matchService.findById(id);
+    }
+}
--- src/match/match.service.ts
+++ src/match/match.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { FirebaseService } from 'src/config/firebase.config';
 
 @Injectable()
 export class MatchService {
+    private db: FirebaseFirestore.Firestore
 
-    findAll() {
-        return;
+    constructor(private readonly firebaseService: FirebaseService) {
+        this.db = firebaseService.getFirestore();
     }
 
-    findById(id: number) {
-        return;
+    async findAll() {
+        const snapshot = await this.db.collection('match').get();
+        return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     }
 
-    createMatch() {
-        return;
+    async findById(id: string) {
+        const doc = await this.db.collection('matches').doc(id).get();
+        return doc.exists ? { id: doc.id, ...doc.data() } : null;
+    }
+
+    async createMatch(data: any) {
+        const docRef = await this.db.collection('match').add(data);
+    
+        return { id: docRef.id, ...data };
     }
 }
