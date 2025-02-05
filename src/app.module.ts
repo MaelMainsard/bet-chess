64a3d270668a90088a2f2d494450397d1964afc0
Necati ÜNLÜ <tati440@hotmail.fr>
2025-02-05 08:24:56 +0100

    ajout matchs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './firebase/firebase.module';
+import { NotificationsModule } from './notifications/notifications.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MatchModule } from './match/match.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MatchModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    FirebaseModule,
+    NotificationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/match/match.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MatchService {
+
+    findAll() {
+        return;
+    }
+
+    findById(id: number) {
+        return;
+    }
+
+    createMatch() {
+        return;
+    }
+}
